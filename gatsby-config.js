f2c285f5f651693b20284b19e22c5dcc969921b5
Andrii S <andriisharapatov@gmail.com>
2021-04-23 18:41:05 +0300

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,17 @@ module.exports = {
         siteUrl: process.env.SITEURL || config.siteUrl,
     },
     plugins: [
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                "UA-180602145", // Google Analytics / GA
+                // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+                // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+              ],
+            },
+          },
         /**
          *  Content Plugins
          */
