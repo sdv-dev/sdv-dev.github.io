23f4604f6778aa7624a33949d74dee90f91898cf
Andrii S <andriisharapatov@gmail.com>
2021-06-15 00:24:55 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,41 +31,45 @@ export default function Footer() {
           <section className="bg-white z-10 relative pt-24">
           <div className="container mx-auto mb-20 md:mb-11">
             <div className="flex flex-col md:flex-row justify-center md:justify-start -mx-4">
-              <div className="flex-shrink w-full md:w-3/12 px-4 mb-10">
-              <Link to={`/`}>
-              <Img
-                  fadeIn={true}
-                  fixed={
-                      data.logo
-                          .childImageSharp.fixed
-                  }
-                  alt="The Synthetic Data Vault Blog"
-                  className=""
-              />
-              </Link>
-                <div className="flex flex-row -mx-1 mt-4">
-                  <div className="px-1">
-                    <Link target="_blank" rel="noopener" 
-                      to="https://sdv-space.slack.com/join/shared_invite/zt-gdsfcb5w-0QQpFMVoyB2Yd6SRiMplcw#/" 
-                      className="w-10 h-10 flex justify-center items-center bg-sdv-mute inline-block rounded-full hover:bg-sdv-graylight bg-sdv-offwhite">
-                      <FontAwesomeIcon width="16" icon={faSlack} />
-                    </Link>
-                  </div>
-                  <div className="px-1">
-                    <Link target="_blank" rel="noopener" 
-                      to="https://twitter.com/sdv_dev" 
-                      className="w-10 h-10 flex justify-center items-center bg-sdv-mute inline-block rounded-full hover:bg-sdv-graylight bg-sdv-offwhite">
-                      <FontAwesomeIcon width="16" icon={faTwitter} />
-                    </Link>
-                  </div>
-                  <div className="px-1">
-                    <Link target="_blank" rel="noopener" 
-                      to="https://www.linkedin.com/company/sdv-dev/" 
-                      className="w-10 h-10 flex justify-center items-center bg-sdv-mute inline-block rounded-full hover:bg-sdv-graylight bg-sdv-offwhite">
-                      <FontAwesomeIcon width="16" icon={faLinkedin} />
-                    </Link>
+              <div className="flex-shrink w-full md:w-3/12 px-4 mb-10 flex justify-center md:justify-start">
+                <div className="w-auto flex-shrink flex flex-col items-center">
+                  <Link to={`/`} className="inline-block">
+                    <Img
+                      fadeIn={true}
+                      fixed={
+                          data.logo
+                              .childImageSharp.fixed
+                      }
+                      alt="The Synthetic Data Vault Blog"
+                      className=""
+                    />
+                  </Link>
+                  <div className="flex flex-row -mx-1 mt-4">
+                    <div className="px-1">
+                      <Link target="_blank" rel="noopener" 
+                        to="https://sdv-space.slack.com/join/shared_invite/zt-gdsfcb5w-0QQpFMVoyB2Yd6SRiMplcw#/" 
+                        className="w-10 h-10 flex justify-center items-center bg-sdv-mute inline-block rounded-full hover:bg-sdv-graylight bg-sdv-offwhite">
+                        <FontAwesomeIcon width="16" icon={faSlack} />
+                      </Link>
+                    </div>
+                    <div className="px-1">
+                      <Link target="_blank" rel="noopener" 
+                        to="https://twitter.com/sdv_dev" 
+                        className="w-10 h-10 flex justify-center items-center bg-sdv-mute inline-block rounded-full hover:bg-sdv-graylight bg-sdv-offwhite">
+                        <FontAwesomeIcon width="16" icon={faTwitter} />
+                      </Link>
+                    </div>
+                    <div className="px-1">
+                      <Link target="_blank" rel="noopener" 
+                        to="https://www.linkedin.com/company/sdv-dev/" 
+                        className="w-10 h-10 flex justify-center items-center bg-sdv-mute inline-block rounded-full hover:bg-sdv-graylight bg-sdv-offwhite">
+                        <FontAwesomeIcon width="16" icon={faLinkedin} />
+                      </Link>
+                    </div>
                   </div>
                 </div>
+              
+                
               </div>
               <div className="flex-grow md:w-9/12 px-4">
                 <div className="flex flex-wrap">
