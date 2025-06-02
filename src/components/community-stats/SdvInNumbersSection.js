526bbfea5c32ac9d8cc600391bdbec5805ce5b70
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-02 10:52:15 +0200

    Add tabs for sdv in numbers section

--- src/components/community-stats/SdvInNumbersSection.js
+++ src/components/community-stats/SdvInNumbersSection.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Tab from "../common/Tab";
 
 export default function SdvInNumbersSection() {
+  const [tabs, setTabs] = useState([
+    { label: "Downloads", isActive: true },
+    { label: "Users", isActive: false },
+    { label: "Visualize", isActive: false },
+  ]);
+
+  const clickTab = (label) => {
+    setTabs((prevTabs) =>
+      prevTabs.map((tab) => ({
+        ...tab,
+        isActive: tab.label === label,
+      }))
+    );
+  };
+
   return (
     <div className="flex justify-center bg-white">
       <div className="container w-full flex flex-col py-12 md:py-16 lg:py-24 px-4 md:px-5 lg:px-0">
@@ -11,10 +27,21 @@ export default function SdvInNumbersSection() {
           href="https://docs.sdv.dev/sdv"
           target="_blank"
           rel="noreferrer"
-          className="text-lg text-blue-600 hover:text-midnight-950 font-semibold duration-200 leading-none text-center cursor-pointer pt-1.5"
+          className="text-lg text-blue-600 hover:text-midnight-950 font-semibold duration-200 leading-none text-center cursor-pointer pt-1.5 pb-12"
         >
           Learn more
         </a>
+        <div className="flex gap-2.5 lg:gap-9 border-b border-b-midnight-200 mb-6">
+          {tabs.map((t) => (
+            <Tab
+              key={t.label}
+              isActive={t.isActive}
+              onClick={() => clickTab(t.label)}
+            >
+              {t.label}
+            </Tab>
+          ))}
+        </div>
       </div>
     </div>
   );
