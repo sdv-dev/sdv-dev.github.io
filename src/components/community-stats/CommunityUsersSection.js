da4206ef8331e2707db1f980e469cf201b4b2e2c
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-05 11:04:57 +0200

    Finalize community users section with mocked data

--- src/components/community-stats/CommunityUsersSection.js
+++ src/components/community-stats/CommunityUsersSection.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import DataceboLogo from "../common/DataceboLogo";
+import sdvLogo from "../../../static/sdv-pie-chart-logo.svg";
 
 export default function CommunityUsersSection() {
-  const slides = [
+  const cards = [
     {
       imgSrc: "",
       title:
@@ -25,32 +26,47 @@ export default function CommunityUsersSection() {
       link: "/",
     },
   ];
-  const [activeSliderIndex] = useState(0);
-  const activeSlide = slides[activeSliderIndex];
 
   return (
     <div className="container w-full flex flex-col py-12 md:py-16 lg:py-24 px-4 md:px-5 lg:px-0">
-      <h2 className="text-midnight-950 text-2xl font-medium leading-[36px] tracking-xs pb-12">
-        Users of community
+      <h2 className="heading-600-lg text-center pb-8 md:pb-12">
+        Users of the Community
       </h2>
-      <div className="flex flex-col md:flex-row">
-        <div className="h-[200px] bg-blue-700 rounded-t-20 md:h-auto md:w-1/2 md:rounded-l-20 md:rounded-tr-none"></div>
-        <div className="flex flex-col md:w-1/2 gap-6 md:gap-12 pb-8 px-6 pt-6 md:pt-[42px] md:px-12 md:pb-12 border-b border-r border-l md:border-l-none md:border-t border-midnight-50 rounded-b-20 md:rounded-bl-none md:rounded-tr-20 bg-midnight-25">
-          <p className="text-midnight-950 text-2xl font-medium leading-[30px] tracking-xs line-clamp-3 min-h-[90px] md:min-h-[120px] md:text-4xl md:-tracking-xl md:leading-10">
-            {activeSlide.title}
-          </p>
-          <div>
-            <a
-              href={activeSlide.link}
-              target="_blank"
-              rel="noreferrer"
-              className="text-lg text-blue-600 hover:text-midnight-950 font-semibold duration-200 leading-none cursor-pointer"
-            >
-              Learn more
-            </a>
-          </div>
-          <DataceboLogo />
-        </div>
+      <hr className="bg-midnight-500 h-0.5 mb-3 md:mb-[18px]" />
+      <div className="flex flex-col">
+        {cards.map((c, idx) => {
+          const isLast = idx === cards.length - 1;
+          return (
+            <div key={c.title}>
+              <div
+                className={`flex flex-col md:flex-row py-8 md:py-12 ${
+                  isLast && "!pb-0"
+                }`}
+              >
+                <div className="flex items-center justify-center h-[200px] bg-blue-700 rounded-t-20 md:h-auto md:w-1/2 md:rounded-l-20 md:rounded-tr-none">
+                  <img src={sdvLogo} alt="sdv logo" />
+                </div>
+                <div className="flex flex-col md:w-1/2 gap-6 md:gap-12 pb-8 px-6 pt-6 md:pt-[42px] md:px-12 md:pb-12 border-b border-r border-l md:border-l-none md:border-t border-midnight-50 rounded-b-20 md:rounded-bl-none md:rounded-tr-20 bg-midnight-25">
+                  <p className="text-midnight-950 text-2xl font-medium leading-[30px] tracking-xs line-clamp-3 min-h-[90px] md:min-h-[120px] md:text-4xl md:-tracking-xl md:leading-10">
+                    {c.title}
+                  </p>
+                  <div>
+                    <a
+                      href={c.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-lg text-blue-600 hover:text-midnight-950 font-semibold duration-200 leading-none cursor-pointer"
+                    >
+                      Learn more
+                    </a>
+                  </div>
+                  <DataceboLogo />
+                </div>
+              </div>
+              {!isLast && <hr className="bg-midnight-100 h-0.5" />}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
