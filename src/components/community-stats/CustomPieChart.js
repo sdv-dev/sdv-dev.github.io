7dd6c290ecd62ca887a1a4ad27bf8dc4b978b062
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-05 14:51:55 +0200

    Adjust pie charts colors and font

--- src/components/community-stats/CustomPieChart.js
+++ src/components/community-stats/CustomPieChart.js
@@ -13,14 +13,14 @@ import useWindowWidth from "../../hooks/useviewport";
 
 const data = [
   { name: "SDV", value: 80.7 },
-  { name: "gretel", value: 10.2 },
+  { name: "Gretel", value: 10.2 },
   { name: "Synthesized", value: 2.8 },
   { name: "YData", value: 2.3 },
   { name: "SynthCity", value: 0.9 },
-  { name: "realtabformer", value: 0.8 },
-  { name: "smartnoise-synth", value: 0.7 },
-  { name: "datomize", value: 0.7 },
-  { name: "be-great", value: 0.7 },
+  { name: "Realtabformer", value: 0.8 },
+  { name: "Smartnoise-synth", value: 0.7 },
+  { name: "Datomize", value: 0.7 },
+  { name: "Be-great", value: 0.7 },
   { name: "MOSTLY AI", value: 0.2 },
 ];
 
@@ -28,13 +28,13 @@ const COLORS = [
   "#01E0C9",
   "#03AFF1",
   "#77DAFF",
-  "#959FBD",
-  "#000036",
+  "#1376B1",
+  "#AEB5CF",
+  "#727C9E",
   "#353E67",
   "#B7E9FF",
-  "#727C9E",
-  "#1376B1",
-  "#525C80",
+  "#0690D1",
+  "#000036",
 ];
 
 export default function CustomPieChart() {
@@ -46,7 +46,7 @@ export default function CustomPieChart() {
       <img
         src={sdvPieChartLogo}
         alt="SDV logo"
-        className="absolute top-[48%] md:top-[50%] z-10"
+        className="absolute top-[42%] md:top-[46%] z-10"
         draggable={false}
       />
       <ResponsiveContainer width={isMobile ? 400 : 600} height={550}>
@@ -71,7 +71,7 @@ export default function CustomPieChart() {
               fontSize={24}
               fontWeight="medium"
               fill="#000036"
-              dy={isMobile ? 130 : 115}
+              dy={isMobile ? 120 : 110}
             />
           </Pie>
           <Tooltip />
@@ -79,7 +79,26 @@ export default function CustomPieChart() {
             layout="horizontal"
             verticalAlign="bottom"
             align="center"
-            formatter={(value, _, index) => `${value} (${data[index].value}%)`}
+            content={({ payload }) => (
+              <ul className="flex flex-wrap justify-center items-center mt-4">
+                {payload.map((entry, index) => (
+                  <li
+                    key={`legend-item-${index}`}
+                    className="flex flex-col items-center text-center mx-[9px] mb-1"
+                  >
+                    <div className="flex items-center">
+                      <span
+                        className="w-3 h-3 mr-2"
+                        style={{ backgroundColor: entry.color }}
+                      />
+                      <span className="text-midnight-950">
+                        {`${entry.value} (${data[index].value}%)`}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
           />
         </PieChart>
       </ResponsiveContainer>
