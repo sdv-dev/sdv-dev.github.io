8a12b0b207ccdd3b58581ae6d39ec207b79af82e
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-09 15:01:14 +0200

    Adjust active tab state text color

--- src/components/common/Tab.js
+++ src/components/common/Tab.js
@@ -5,7 +5,7 @@ export default function Tab({ children, isActive, onClick }) {
     <div
       onClick={onClick}
       className={`text-midnight-800 text-base md:text-lg lg:text-xl font-semibold leading-none md:tracking-lg cursor-pointer py-4 px-5 ${
-        isActive ? "bg-[#070C2F] rounded-[40px] text-midnight-0" : ""
+        isActive ? "bg-[#070C2F] rounded-[40px] !text-midnight-0" : ""
       }`}
     >
       {children}
