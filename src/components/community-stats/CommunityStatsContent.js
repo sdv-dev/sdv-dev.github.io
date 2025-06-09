b8255484cbd7dcd7576bc3ced61ec7cc33ef1480
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-09 12:05:32 +0200

    Fetch data from excel for sdv in numbers section

--- src/components/community-stats/CommunityStatsContent.js
+++ src/components/community-stats/CommunityStatsContent.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as XLSX from "xlsx";
 import SdvCommunityHero from "./SdvCommunityHero";
 import NewsSliderSection from "./NewsSliderSection";
 import SynthesizeCardsSection from "./SynthesizeCardsSection";
@@ -9,12 +10,64 @@ import SdvOpenCoreSection from "./SdvOpenCoreSection";
 import BannerSection from "./BannerSection";
 
 export default function CommunityStatsContent() {
+  const [data, setData] = useState([]);
+  const fileUrl =
+    "https://raw.githubusercontent.com/sdv-dev/sdv-dev.github.io/gatsby-home/assets/GitHub_Summary.xlsx";
+
+  useEffect(() => {
+    async function fetchExcel() {
+      try {
+        const response = await fetch(fileUrl);
+        const blob = await response.blob();
+        const arrayBuffer = await blob.arrayBuffer();
+        const workbook = XLSX.read(arrayBuffer, { type: "array" });
+
+        const firstSheet = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheet];
+        const rawData = XLSX.utils.sheet_to_json(worksheet);
+
+        function formatThousands(value) {
+          if (value >= 1_000_000)
+            return (value / 1_000_000).toFixed(2).replace(/\.00$/, "") + "M";
+          if (value >= 1_000) return (value / 1_000).toFixed(0) + "K";
+          return value.toString();
+        }
+
+        const filtered = rawData.filter(
+          (item) =>
+            item["Total Since Beginning"] !== undefined &&
+            item["2025"] !== undefined
+        );
+
+        const total = filtered.reduce(
+          (sum, item) => sum + item["Total Since Beginning"],
+          0
+        );
+
+        const enrichedData = filtered.map((item) => ({
+          name: item.Ecosystem,
+          percentage: parseFloat(
+            ((item["Total Since Beginning"] / total) * 100).toFixed(2)
+          ),
+          toDate: formatThousands(item["Total Since Beginning"] * 1000),
+          yearToDate: formatThousands(item["2025"] * 1000),
+        }));
+
+        setData(enrichedData);
+      } catch (error) {
+        console.error("Error loading Excel file:", error);
+      }
+    }
+
+    fetchExcel();
+  }, []);
+
   return (
     <div className="pt-16 relative bg-white flex flex-col justify-center overflow-hidden">
       <SdvCommunityHero />
       <SynthesizeCardsSection />
       <CommunityUsersSection />
-      <SdvInNumbersSection />
+      <SdvInNumbersSection data={data} />
       <SdvCoreSection />
       <SdvOpenCoreSection />
       <NewsSliderSection />
