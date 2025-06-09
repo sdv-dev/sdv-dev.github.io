6819444f61c64e89ca2c919f1c1b37ce961abf01
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-09 12:22:30 +0200

    Enlarge pie chart and adjust the dimensions

--- src/components/community-stats/CustomPieChart.js
+++ src/components/community-stats/CustomPieChart.js
@@ -19,24 +19,35 @@ const COLORS = [
 const renderCustomizedLabel = ({ name, value, index }) => {
   const labelPositions = [
     { x: -290, y: -450 },
-    { x: 90, y: 130 },
-    { x: 90, y: 160 },
-    { x: 90, y: 190 },
-    { x: 630, y: 100 },
-    { x: 650, y: 130 },
-    { x: 650, y: 160 },
-    { x: 650, y: 190 },
+    { x: 80, y: 100 },
+    { x: 80, y: 130 },
+    { x: 80, y: 160 },
+    { x: 670, y: 100 },
+    { x: 710, y: 130 },
+    { x: 720, y: 160 },
+    { x: 730, y: 190 },
   ];
 
   const lineCoordinates = [
     { from: [-270, -450], to: [-290, -500] },
-    { from: [310, 130], to: [170, 130] },
-    { from: [365, 160], to: [150, 160] },
-    { from: [400, 190], to: [155, 190] },
-    { from: [430, 100], to: [570, 100] },
-    { from: [450, 130], to: [570, 130] },
-    { from: [450, 160], to: [580, 160] },
-    { from: [445, 190], to: [590, 190] },
+    { from: [310, 100], to: [170, 100] },
+    { from: [365, 130], to: [150, 130] },
+    { from: [400, 160], to: [140, 160] },
+    { from: [430, 100], to: [620, 100] },
+    { from: [450, 130], to: [640, 130] },
+    { from: [450, 160], to: [660, 160] },
+    { from: [445, 190], to: [670, 190] },
+  ];
+
+  const strokeColors = [
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
+    "rgba(227, 234, 255, 0.93)",
   ];
 
   const label = labelPositions[index];
@@ -50,7 +61,7 @@ const renderCustomizedLabel = ({ name, value, index }) => {
         <>
           <polyline
             points={`${line.from[0]},${line.from[1]} ${line.to[0]},${line.to[1]}`}
-            stroke="rgba(227, 234, 255, 0.93)"
+            stroke={strokeColors[index]}
             strokeWidth={2}
             fill="none"
           />
@@ -58,7 +69,7 @@ const renderCustomizedLabel = ({ name, value, index }) => {
             cx={line.from[0]}
             cy={line.from[1]}
             r={3}
-            fill="rgba(227, 234, 255, 0.93)"
+            fill={strokeColors[index]}
           />
         </>
       )}
@@ -80,14 +91,13 @@ export default function CustomPieChart({ data }) {
   const width = useWindowWidth();
   const isMobile = width < 768;
   const sortedData = [...data].sort((a, b) => b.percentage - a.percentage);
-  console.log(sortedData);
 
   return (
     <div className="w-full flex flex-col items-center relative">
       <img
         src={sdvPieChartLogo}
         alt="SDV logo"
-        className="absolute top-[42%] md:top-[54%] z-10"
+        className="absolute top-[42%] md:top-[55%] z-10"
         draggable={false}
       />
       <PieChart width={isMobile ? 400 : 800} height={550}>
@@ -95,7 +105,7 @@ export default function CustomPieChart({ data }) {
           data={sortedData}
           cx="50%"
           cy="50%"
-          outerRadius={isMobile ? 180 : 200}
+          outerRadius={isMobile ? 180 : 275}
           dataKey="percentage"
           startAngle={60}
           endAngle={-300}
@@ -110,10 +120,10 @@ export default function CustomPieChart({ data }) {
           <Label
             value={`${sortedData[0]?.percentage}%`}
             position="center"
-            fontSize={24}
+            fontSize={isMobile ? 28 : 38}
             fontWeight="medium"
             fill="#000036"
-            dy={isMobile ? 120 : 110}
+            dy={isMobile ? 110 : 120}
           />
         </Pie>
         {isMobile && (
@@ -121,9 +131,9 @@ export default function CustomPieChart({ data }) {
             layout="horizontal"
             verticalAlign="bottom"
             align="center"
-            content={({ payload }) => (
+            content={() => (
               <ul className="flex flex-wrap justify-center items-center mt-4">
-                {payload.map((entry, index) => (
+                {sortedData.map((entry, index) => (
                   <li
                     key={`legend-item-${index}`}
                     className="flex flex-col items-center text-center mx-[9px] mb-1"
@@ -131,10 +141,10 @@ export default function CustomPieChart({ data }) {
                     <div className="flex items-center">
                       <span
                         className="w-3 h-3 mr-2"
-                        style={{ backgroundColor: entry.color }}
+                        style={{ backgroundColor: COLORS[index] }}
                       />
                       <span className="text-midnight-950">
-                        {`${entry.percentage} (${sortedData[index].percentage}%)`}
+                        {`${entry.name} (${sortedData[index].percentage}%)`}
                       </span>
                     </div>
                   </li>
