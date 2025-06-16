eb86ca25f7d9920c5230f0cb01878d9f18abb424
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-16 12:10:40 +0200

    Add logos for community users section

--- src/components/community-stats/CommunityUsersSection.js
+++ src/components/community-stats/CommunityUsersSection.js
@@ -1,27 +1,34 @@
 import React from "react";
 import DataceboLogo from "../common/DataceboLogo";
-import sdvLogo from "../../../static/sdv-pie-chart-logo.svg";
+import KaggleLogo from "./KaggleLogo";
+import MapfreeLogo from "./MapfreeLogo";
+import JpmcLogo from "./JpmcLogo";
+import SparNordLogo from "./SparNordLogo";
 
 export default function CommunityUsersSection() {
   const cards = [
     {
-      imgSrc: "",
+      logo: <JpmcLogo />,
+      bg: "#0079C1",
       title:
         "JP Morgan Chase lorem ipsum dolorem lorem ipsum dolor lorem ipsum dolor dolor dolor dolor",
       link: "/",
     },
     {
-      imgSrc: "",
+      logo: <MapfreeLogo />,
+      bg: "#BE002F",
       title: "MAPFRE insurance improves fraud detection by xx% using SDV",
       link: "/",
     },
     {
-      imgSrc: "",
+      logo: <KaggleLogo />,
+      bg: "#20BEFF",
       title: "Kaggle releases data for 11 competitions using SDV",
       link: "/",
     },
     {
-      imgSrc: "",
+      logo: <SparNordLogo />,
+      bg: "#1D427D",
       title: "Spar Nord Bank is  using SDV",
       link: "/",
     },
@@ -44,8 +51,11 @@ export default function CommunityUsersSection() {
                   (isLast && "!pb-0", isFirst && "!pt-0")
                 }`}
               >
-                <div className="flex items-center justify-center h-[200px] bg-blue-700 rounded-t-20 md:h-auto md:w-1/2 md:rounded-l-20 md:rounded-tr-none">
-                  <img src={sdvLogo} alt="sdv logo" />
+                <div
+                  className="flex items-center justify-center h-[200px] rounded-t-20 md:h-auto md:w-1/2 md:rounded-l-20 md:rounded-tr-none"
+                  style={{ backgroundColor: c.bg }}
+                >
+                  {c.logo}
                 </div>
                 <div className="flex flex-col md:w-1/2 gap-6 md:gap-12 pb-8 px-6 pt-6 md:pt-[42px] md:px-12 md:pb-12 border-b border-r border-l md:border-l-none md:border-t border-midnight-50 rounded-b-20 md:rounded-bl-none md:rounded-tr-20 bg-midnight-25">
                   <p className="text-midnight-950 text-2xl font-medium leading-[30px] tracking-xs line-clamp-3 min-h-[90px] md:min-h-[120px] md:text-4xl md:-tracking-xl md:leading-10">
