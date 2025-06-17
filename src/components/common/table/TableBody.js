572c5242e34fc154acf1ae3eec60ea1f054a2623
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-17 13:28:27 +0200

    Adjust table UI

--- src/components/common/table/TableBody.js
+++ src/components/common/table/TableBody.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function TableBody(props) {
-  return <div style={{ gridArea: "body" }}>{props.children}</div>;
+  return (
+    <div style={{ gridArea: "body" }}>
+      <div className="h-1.5 bg-white"></div>
+      {props.children}
+    </div>
+  );
 }
--- src/components/common/table/TableHeaderCell.js
+++ src/components/common/table/TableHeaderCell.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TableHeaderCell({ children }) {
   return (
-    <div className="flex justify-end py-[33px] pl-6 pr-8 font-semibold text-[20px] text-midnight-950 leading-none tracking-lg">
+    <div className="flex justify-end py-[27px] pl-6 pr-8 font-semibold text-[20px] text-midnight-950 leading-none tracking-lg">
       {children}
     </div>
   );
