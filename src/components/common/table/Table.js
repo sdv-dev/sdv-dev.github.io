cb2615c291aea44da4d766c031661ec2f65d4daa
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-19 12:57:08 +0200

    Adjust the table gradient

--- src/components/common/table/Table.js
+++ src/components/common/table/Table.js
@@ -11,7 +11,7 @@ export default function Table({ children, tableColDimensions }) {
     <div
       className="border border-midnight-50 rounded-[36px] p-5 md:p-9"
       style={{
-        background: `conic-gradient(from 0deg at 50% 50%, var(--primary-dark-midnight-0, rgba(255, 255, 255, 0.14)) 0.15417183400131762deg, rgba(234, 252, 251, 0.14) 67.58502066135406deg, var(--primary-teal-400, rgba(1, 224, 201, 0.14)) 126.67272806167603deg, var(--primary-blue-500, rgba(3, 175, 241, 0.14)) 232.73083448410034deg, rgba(236, 249, 254, 0.14) 295.11245012283325deg), var(--primary-dark-midnight-25, #F6F6F9)`,
+        background: `conic-gradient(from 0deg at 50% 50%, var(--primary-dark-midnight-0, rgba(255, 255, 255, 0.10)) 0.15417183400131762deg, rgba(234, 252, 251, 0.10) 67.58502066135406deg, var(--primary-teal-400, rgba(1, 224, 201, 0.10)) 126.67272806167603deg, var(--primary-blue-500, rgba(3, 175, 241, 0.10)) 232.73083448410034deg, rgba(236, 249, 254, 0.10) 295.11245012283325deg), var(--primary-dark-midnight-25, #F6F6F9)`,
       }}
     >
       <div
