46e36fe2ca4a8136d8707160433ddbf007c69d0f
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-23 12:41:30 +0200

    Configure Ghost page route in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,39 +1,68 @@
-const path = require(`path`)
-const { postsPerPage } = require(`./src/utils/siteConfig`)
-const { paginate } = require(`gatsby-awesome-pagination`)
-const util = require('util');
-const glob = require("glob")
-const fs = require('fs');
-const md5 = require('md5');
-const hash = md5(`${new Date().getTime()}`)
+const path = require(`path`);
+const { postsPerPage } = require(`./src/utils/siteConfig`);
+const { paginate } = require(`gatsby-awesome-pagination`);
+const util = require("util");
+const glob = require("glob");
+const fs = require("fs");
+const md5 = require("md5");
+const hash = md5(`${new Date().getTime()}`);
 
-const addPageDataVersion = async file => {
-  const stats = await util.promisify(fs.stat)(file)
+const addPageDataVersion = async (file) => {
+  const stats = await util.promisify(fs.stat)(file);
   if (stats.isFile()) {
-    console.log(`Adding version to page-data.json in ${file}..`)
-    let content = await util.promisify(fs.readFile)(file, 'utf8')
+    console.log(`Adding version to page-data.json in ${file}..`);
+    let content = await util.promisify(fs.readFile)(file, "utf8");
     const result = content.replace(
       /page-data.json(\?v=[a-f0-9]{32})?/g,
       `page-data.json?v=${hash}`
-    )
-    await util.promisify(fs.writeFile)(file, result, 'utf8')
+    );
+    await util.promisify(fs.writeFile)(file, result, "utf8");
   }
-}
+};
 
 exports.onPostBootstrap = async () => {
-  const loader = path.join(__dirname, 'node_modules/gatsby/cache-dir/loader.js')
-  await addPageDataVersion(loader)
-}
+  const loader = path.join(
+    __dirname,
+    "node_modules/gatsby/cache-dir/loader.js"
+  );
+  await addPageDataVersion(loader);
+};
 
 exports.onPostBuild = async () => {
-  const publicPath = path.join(__dirname, 'public')
-  const htmlAndJSFiles = glob.sync(`${publicPath}/**/*.{html,js}`)
+  const publicPath = path.join(__dirname, "public");
+  const htmlAndJSFiles = glob.sync(`${publicPath}/**/*.{html,js}`);
   for (let file of htmlAndJSFiles) {
-    await addPageDataVersion(file)
+    await addPageDataVersion(file);
   }
-}
+};
 
 /**
  * Here is the place where Gatsby creates the URLs for all the
  * posts, tags, pages and authors that we fetched from the Ghost site.
  */
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allGhostPost(
+        filter: { tags: { elemMatch: { slug: { eq: "case-study" } } } }
+      ) {
+        nodes {
+          slug
+        }
+      }
+    }
+  `);
+
+  result.data.allGhostPost.nodes.forEach((post) => {
+    createPage({
+      path: `/community-case-studies/${post.slug}/`,
+      component: require.resolve(`./src/templates/CaseStudy.js`),
+      context: {
+        slug: post.slug,
+      },
+    });
+  });
+};
---
+++ src/templates/CaseStudy.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CaseStudy = () => {
+  return <div>CaseStudy</div>;
+};
+
+export default CaseStudy;
