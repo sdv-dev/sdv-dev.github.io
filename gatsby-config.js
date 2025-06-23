5eaffc712a4b4a924181c2790cf66947ef655a6e
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-23 12:12:10 +0200

    Install gatsby ghost plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,153 +1,153 @@
-const path = require(`path`)
+const path = require(`path`);
 
-const config = require(`./src/utils/siteConfig`)
+const config = require(`./src/utils/siteConfig`);
 
 /**
-* This is the place where you can tell Gatsby which plugins to use
-* and set them up the way you want.
-*
-* Further info 👉🏼 https://www.gatsbyjs.org/docs/gatsby-config/
-*
-*/
+ * This is the place where you can tell Gatsby which plugins to use
+ * and set them up the way you want.
+ *
+ * Further info 👉🏼 https://www.gatsbyjs.org/docs/gatsby-config/
+ *
+ */
 module.exports = {
-    pathPrefix: "/",
-    siteMetadata: {
-        siteUrl: process.env.SITEURL || config.siteUrl,
-        url: process.env.SITEURL || config.siteUrl,
-        twitterUsername: "@sdv_dev",
-        image: config.image,
+  pathPrefix: "/",
+  siteMetadata: {
+    siteUrl: process.env.SITEURL || config.siteUrl,
+    url: process.env.SITEURL || config.siteUrl,
+    twitterUsername: "@sdv_dev",
+    image: config.image,
+  },
+  flags: {
+    // PRESERVE_WEBPACK_CACHE: true,
+    // PRESERVE_FILE_DOWNLOAD_CACHE: true,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-VS2E5NF00K", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
     },
-    flags: {
-        // PRESERVE_WEBPACK_CACHE: true,
-        // PRESERVE_FILE_DOWNLOAD_CACHE: true,
+    /**
+     *  Content Plugins
+     */
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.join(__dirname, `src`, `pages`),
+        name: `pages`,
+      },
     },
-    plugins: [
-        {
-            resolve: `gatsby-plugin-google-gtag`,
-            options: {
-              // You can add multiple tracking ids and a pageview event will be fired for all of them.
-              trackingIds: [
-                "G-VS2E5NF00K", // Google Analytics / GA
-                // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-                // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-              ],
-            },
-          },
-        /**
-         *  Content Plugins
-         */
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: path.join(__dirname, `src`, `pages`),
-                name: `pages`,
-            },
+    // Setup for optimised images.
+    // See https://www.gatsbyjs.org/packages/gatsby-image/
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.join(__dirname, `src`, `images`),
+        name: `images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `static`),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
         },
-        // Setup for optimised images.
-        // See https://www.gatsbyjs.org/packages/gatsby-image/
-        {
-            resolve: `gatsby-source-filesystem`,
+      },
+    },
+    `gatsby-plugin-scroll-reveal`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
             options: {
-                path: path.join(__dirname, `src`, `images`),
-                name: `images`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-              name: `images`,
-              path: path.join(__dirname, `static`),
-            },
-        },
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-              rule: {
-                include: /assets/, // See below to configure properly
-              },
-            },
-        },
-        `gatsby-plugin-scroll-reveal`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            // Class prefix for <pre> tags containing syntax highlighting;
-            // defaults to 'language-' (e.g. <pre class="language-js">).
-            // If your site loads Prism into the browser at runtime,
-            // (e.g. for use with libraries like react-live),
-            // you may use this to prevent Prism from re-processing syntax.
-            // This is an uncommon use-case though;
-            // If you're unsure, it's best to use the default value.
-            classPrefix: "language-",
-            // This is used to allow setting a language for inline code
-            // (i.e. single backticks) by creating a separator.
-            // This separator is a string and will do no white-space
-            // stripping.
-            // A suggested value for English speakers is the non-ascii
-            // character '›'.
-            inlineCodeMarker: null,
-            // This lets you set up language aliases.  For example,
-            // setting this to '{ sh: "bash" }' will let you use
-            // the language "sh" which will highlight using the
-            // bash highlighter.
-            aliases: {},
-            // This toggles the display of line numbers globally alongside the code.
-            // To use it, add the following line in gatsby-browser.js
-            // right after importing the prism color scheme:
-            //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-            // Defaults to false.
-            // If you wish to only show line numbers on certain code blocks,
-            // leave false and use the {numberLines: true} syntax below
-            showLineNumbers: false,
-            // If setting this to true, the parser won't handle and highlight inline
-            // code used in markdown i.e. single backtick code like `this`.
-            noInlineHighlight: false,
-            // This adds a new language definition to Prism or extend an already
-            // existing language definition. More details on this option can be
-            // found under the header "Add new language definition or extend an
-            // existing language" below.
-            languageExtensions: [
-              {
-                language: "superscript",
-                extend: "javascript",
-                definition: {
-                  superscript_types: /(SuperType)/,
-                },
-                insertBefore: {
-                  function: {
-                    superscript_keywords: /(superif|superelse)/,
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (e.g. <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (e.g. for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: "language-",
+              // This is used to allow setting a language for inline code
+              // (i.e. single backticks) by creating a separator.
+              // This separator is a string and will do no white-space
+              // stripping.
+              // A suggested value for English speakers is the non-ascii
+              // character '›'.
+              inlineCodeMarker: null,
+              // This lets you set up language aliases.  For example,
+              // setting this to '{ sh: "bash" }' will let you use
+              // the language "sh" which will highlight using the
+              // bash highlighter.
+              aliases: {},
+              // This toggles the display of line numbers globally alongside the code.
+              // To use it, add the following line in gatsby-browser.js
+              // right after importing the prism color scheme:
+              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+              // Defaults to false.
+              // If you wish to only show line numbers on certain code blocks,
+              // leave false and use the {numberLines: true} syntax below
+              showLineNumbers: false,
+              // If setting this to true, the parser won't handle and highlight inline
+              // code used in markdown i.e. single backtick code like `this`.
+              noInlineHighlight: false,
+              // This adds a new language definition to Prism or extend an already
+              // existing language definition. More details on this option can be
+              // found under the header "Add new language definition or extend an
+              // existing language" below.
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
                   },
                 },
+              ],
+              // Customize the prompt used in shell output
+              // Values below are default
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false,
               },
-            ],
-            // Customize the prompt used in shell output
-            // Values below are default
-            prompt: {
-              user: "root",
-              host: "localhost",
-              global: false,
+              // By default the HTML entities <>&'" are escaped.
+              // Add additional HTML escapes by providing a mapping
+              // of HTML entities and their escape value IE: { '}': '&#123;' }
+              escapeEntities: {},
             },
-            // By default the HTML entities <>&'" are escaped.
-            // Add additional HTML escapes by providing a mapping
-            // of HTML entities and their escape value IE: { '}': '&#123;' }
-            escapeEntities: {},
           },
-        },
-      ],
+        ],
+      },
     },
-  },
-        `gatsby-plugin-styled-components`,
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-force-trailing-slashes`,
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-anchor-links`
-    ],
-}
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-force-trailing-slashes`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-anchor-links`,
+  ],
+};
