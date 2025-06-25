fd888466f2fb2a1e286b428dac5e6c7b6998dc34
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-25 13:17:18 +0200

    Update tag slug and format yml

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,7 +47,9 @@ exports.createPages = async ({ actions, graphql }) => {
   const result = await graphql(`
     {
       allGhostPost(
-        filter: { tags: { elemMatch: { slug: { eq: "hash-case-study" } } } }
+        filter: {
+          tags: { elemMatch: { slug: { eq: "hash-community-case-study" } } }
+        }
       ) {
         nodes {
           slug
