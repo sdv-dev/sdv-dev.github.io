d7cec4c873f6fba4d2b103c1bedd6975bcd67b44
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-26 14:07:59 +0200

    Add news slider and banner section to sd gym

--- src/components/sd-gym/SdGymContent.js
+++ src/components/sd-gym/SdGymContent.js
@@ -1,10 +1,14 @@
 import React from "react";
 import SdGymHero from "./SdGymHero";
+import NewsSliderSection from "../community-stats/NewsSliderSection";
+import BannerSection from "../community-stats/BannerSection";
 
 export default function SdGymContent() {
   return (
     <div className="pt-16 relative bg-white flex flex-col justify-center overflow-hidden">
       <SdGymHero />
+      <NewsSliderSection />
+      <BannerSection />
     </div>
   );
 }
