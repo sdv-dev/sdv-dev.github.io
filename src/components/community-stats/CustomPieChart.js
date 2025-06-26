e4052e9fa345cc22a34d2b982b56867fe50731c3
pcarapic15 <predrag.carapic@bytex.ro>
2025-06-26 10:41:24 +0200

    Update pie chart dots position

--- src/components/community-stats/CustomPieChart.js
+++ src/components/community-stats/CustomPieChart.js
@@ -74,12 +74,12 @@ const renderCustomizedLabel = ({ name, value, index, isTablet, isMobile }) => {
       to: [isTablet ? 69 : 56, isTablet ? 240 : 190],
     },
     {
-      from: [isTablet ? 457 : 557, isTablet ? 100 : 100],
+      from: [isTablet ? 457 : 558, isTablet ? 100 : 100],
       bend: [isTablet ? 320 : 220, isTablet ? 310 : 260],
       to: [isTablet ? 71 : 57, isTablet ? 310 : 260],
     },
     {
-      from: [isTablet ? 465 : 583, isTablet ? 100 : 50],
+      from: [isTablet ? 465 : 584, isTablet ? 100 : 50],
       bend: [isTablet ? 600 : 740, isTablet ? 100 : 50],
       to: [isTablet ? 742 : 944, isTablet ? 100 : 50],
     },
