1768a56237c455be1ce3660040bd45e126b0ea9d
pcarapic15 <predrag.carapic@bytex.ro>
2025-07-11 09:51:00 +0200

    Hide sd-gym temporarily

--- src/pages/sd-gym.js
+++ src/pages/sd-gym.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { Article } from "../components/common";
-import config from "../utils/siteConfig";
-import Seo from "../components/Seo";
-import SdGymContent from "../components/sd-gym/SdGymContent";
+// import { Article } from "../components/common";
+// import config from "../utils/siteConfig";
+// import Seo from "../components/Seo";
+// import SdGymContent from "../components/sd-gym/SdGymContent";
+import NotFoundPage from "./404";
 
 export default function SdGymPage() {
-  return (
-    <Article>
-      <Seo
-        title={config.siteTitleMeta}
-        description={config.siteDescriptionMeta}
-        type="website"
-        image={`/sdv-home.jpg`}
-        canonical={`https://sdv.dev/`}
-      />
+  return <NotFoundPage />;
 
-      <div className="mx-auto">
-        <SdGymContent />
-      </div>
-    </Article>
-  );
+  // return (
+  //   <Article>
+  //     <Seo
+  //       title={config.siteTitleMeta}
+  //       description={config.siteDescriptionMeta}
+  //       type="website"
+  //       image={`/sdv-home.jpg`}
+  //       canonical={`https://sdv.dev/`}
+  //     />
+
+  //     <div className="mx-auto">
+  //       <SdGymContent />
+  //     </div>
+  //   </Article>
+  // );
 }
