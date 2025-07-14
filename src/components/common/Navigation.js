6259bf13232f3138c5f3513785d960a2b6379a5a
pcarapic15 <predrag.carapic@bytex.ro>
2025-07-14 17:25:44 +0200

    Address feedback for tables and naming

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -165,7 +165,7 @@ const Navigation = ({ navClass, children, isDark }) => {
                         className="text-midnight-900 text-center font-semibold inline-block w-full px-6 lg:px-5 py-4 lg:py-2 lg:border border-b border-midnight-50 lg:rounded-lg lg:bg-[rgba(30,30,105,0.04)] group-hover:bg-[#0505580f] group-active:bg-[#132e891c]"
                         href="https://datacebo.com/contact"
                       >
-                        Contact us
+                        Contact Us
                       </a>
                     </li>
                   )}
