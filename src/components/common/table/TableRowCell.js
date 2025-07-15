aa8d53a2b0a2f6736b7cba9eec916c2ff187618b
pcarapic15 <predrag.carapic@bytex.ro>
2025-07-15 10:17:37 +0200

    Change dimensions in table

--- src/components/common/table/TableRowCell.js
+++ src/components/common/table/TableRowCell.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TableRowCell({ children }) {
   return (
-    <div className="py-[15px] pl-6 pr-8 font-normal text-lg text-midnight-800 leading-none">
+    <div className="py-[15px] pl-6 pr-6 md:pr-8 font-normal text-lg text-midnight-800 leading-none">
       {children}
     </div>
   );
