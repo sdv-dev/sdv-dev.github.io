10c9605ae3d20bc5259139f000aec41d2bcaa7a9
pcarapic15 <predrag.carapic@bytex.ro>
2025-07-18 09:59:45 +0200

    Add mobile version for pie chart

--- src/components/community-stats/CustomPieChart.js
+++ src/components/community-stats/CustomPieChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { PieChart, Pie, Cell, Label, Legend } from "recharts";
+import { PieChart, Pie, Cell, Label } from "recharts";
 import sdvPieChartLogo from "../../../static/sdv-pie-chart-logo.svg";
 import gretelSvg from "../../../static/gretel.svg";
 import useWindowWidth from "../../hooks/useviewport";
@@ -32,76 +32,184 @@ const renderCustomizedLabel = ({ name, value, index, isTablet, isMobile }) => {
   ];
   const gradientOffsets = [
     100,
-    isTablet ? 31 : 27,
-    isTablet ? 60.5 : 53,
-    isTablet ? 72.5 : 63.5,
-    isTablet ? 77.5 : 69,
-    isTablet ? 24.5 : 20.5,
-    isTablet ? 48 : 39,
-    isTablet ? 58.5 : 50,
-    isTablet ? 65.5 : 58.5,
-    isTablet ? 67.5 : 62.5,
+    isMobile ? 21 : isTablet ? 31 : 27,
+    isMobile ? 60 : isTablet ? 60.5 : 53,
+    isMobile ? 73 : isTablet ? 72.5 : 63.5,
+    isMobile ? 75.5 : isTablet ? 77.5 : 69,
+    isMobile ? 5.5 : isTablet ? 24.5 : 20.5,
+    isMobile ? 47 : isTablet ? 48 : 39,
+    isMobile ? 66 : isTablet ? 58.5 : 50,
+    isMobile ? 72 : isTablet ? 65.5 : 58.5,
+    isMobile ? 69 : isTablet ? 67.5 : 62.5,
   ];
 
   const labelPositions = [
     { x: -290, y: -450 },
-    { x: isTablet ? 65 : 50, y: isTablet ? 100 : 50 },
-    { x: isTablet ? 67 : 50, y: isTablet ? 170 : 120 },
-    { x: isTablet ? 68 : 51, y: isTablet ? 240 : 190 },
-    { x: isTablet ? 71 : 51, y: isTablet ? 310 : 260 },
-    { x: isTablet ? 743 : 945, y: isTablet ? 100 : 50 },
-    { x: isTablet ? 745 : 945, y: isTablet ? 170 : 120 },
-    { x: isTablet ? 744 : 945, y: isTablet ? 240 : 190 },
-    { x: isTablet ? 744 : 945, y: isTablet ? 310 : 260 },
-    { x: isTablet ? 744 : 945, y: isTablet ? 380 : 330 },
+    {
+      x: isMobile ? 2 : isTablet ? 65 : 50,
+      y: isMobile ? 40 : isTablet ? 100 : 50,
+    },
+    {
+      x: isMobile ? 3 : isTablet ? 67 : 50,
+      y: isMobile ? 90 : isTablet ? 170 : 120,
+    },
+    {
+      x: isMobile ? 4 : isTablet ? 68 : 51,
+      y: isMobile ? 140 : isTablet ? 240 : 190,
+    },
+    {
+      x: isMobile ? 5 : isTablet ? 71 : 51,
+      y: isMobile ? 190 : isTablet ? 310 : 260,
+    },
+    {
+      x: isMobile ? 374 : isTablet ? 743 : 945,
+      y: isMobile ? 40 : isTablet ? 100 : 50,
+    },
+    {
+      x: isMobile ? 374 : isTablet ? 745 : 945,
+      y: isMobile ? 90 : isTablet ? 170 : 120,
+    },
+    {
+      x: isMobile ? 373 : isTablet ? 744 : 945,
+      y: isMobile ? 140 : isTablet ? 240 : 190,
+    },
+    {
+      x: isMobile ? 372 : isTablet ? 744 : 945,
+      y: isMobile ? 190 : isTablet ? 310 : 260,
+    },
+    {
+      x: isMobile ? 372 : isTablet ? 744 : 945,
+      y: isMobile ? 240 : isTablet ? 380 : 330,
+    },
   ];
 
   const polylinePaths = [
     { from: [-270, -450], bend: [-290, -475], to: [-290, -500] },
     {
-      from: [isTablet ? 360 : 453, isTablet ? 100 : 50],
-      bend: [isTablet ? 300 : 370, isTablet ? 100 : 50],
-      to: [isTablet ? 66 : 50, isTablet ? 100 : 50],
+      from: [
+        isMobile ? 165 : isTablet ? 360 : 453,
+        isMobile ? 40 : isTablet ? 100 : 50,
+      ],
+      bend: [
+        isMobile ? 5 : isTablet ? 300 : 370,
+        isMobile ? 40 : isTablet ? 100 : 50,
+      ],
+      to: [
+        isMobile ? 50 : isTablet ? 66 : 50,
+        isMobile ? 40 : isTablet ? 100 : 50,
+      ],
     },
     {
-      from: [isTablet ? 410 : 516, isTablet ? 115 : 60],
-      bend: [isTablet ? 320 : 250, isTablet ? 170 : 122],
-      to: [isTablet ? 69 : 53, isTablet ? 170 : 122],
+      from: [
+        isMobile ? 195 : isTablet ? 410 : 516,
+        isMobile ? 50 : isTablet ? 115 : 60,
+      ],
+      bend: [
+        isMobile ? 85 : isTablet ? 320 : 250,
+        isMobile ? 90 : isTablet ? 170 : 122,
+      ],
+      to: [
+        isMobile ? 5 : isTablet ? 69 : 53,
+        isMobile ? 90 : isTablet ? 170 : 122,
+      ],
     },
     {
-      from: [isTablet ? 440 : 545, isTablet ? 110 : 80],
-      bend: [isTablet ? 320 : 220, isTablet ? 240 : 190],
-      to: [isTablet ? 69 : 56, isTablet ? 240 : 190],
+      from: [
+        isMobile ? 208 : isTablet ? 440 : 545,
+        isMobile ? 60 : isTablet ? 110 : 80,
+      ],
+      bend: [
+        isMobile ? 70 : isTablet ? 320 : 220,
+        isMobile ? 140 : isTablet ? 240 : 190,
+      ],
+      to: [
+        isMobile ? 5 : isTablet ? 69 : 56,
+        isMobile ? 140 : isTablet ? 240 : 190,
+      ],
     },
     {
-      from: [isTablet ? 457 : 558, isTablet ? 100 : 100],
-      bend: [isTablet ? 320 : 220, isTablet ? 310 : 260],
-      to: [isTablet ? 71 : 57, isTablet ? 310 : 260],
+      from: [
+        isMobile ? 215 : isTablet ? 457 : 558,
+        isMobile ? 65 : isTablet ? 100 : 100,
+      ],
+      bend: [
+        isMobile ? 74 : isTablet ? 320 : 220,
+        isMobile ? 190 : isTablet ? 310 : 260,
+      ],
+      to: [
+        isMobile ? 5 : isTablet ? 71 : 57,
+        isMobile ? 190 : isTablet ? 310 : 260,
+      ],
     },
     {
-      from: [isTablet ? 465 : 584, isTablet ? 100 : 50],
-      bend: [isTablet ? 600 : 740, isTablet ? 100 : 50],
-      to: [isTablet ? 742 : 944, isTablet ? 100 : 50],
+      from: [
+        isMobile ? 228 : isTablet ? 465 : 584,
+        isMobile ? 40 : isTablet ? 100 : 50,
+      ],
+      bend: [
+        isMobile ? 228 : isTablet ? 600 : 740,
+        isMobile ? 40 : isTablet ? 100 : 50,
+      ],
+      to: [
+        isMobile ? 373 : isTablet ? 742 : 944,
+        isMobile ? 40 : isTablet ? 100 : 50,
+      ],
     },
     {
-      from: [isTablet ? 471 : 588, isTablet ? 105 : 65],
-      bend: [isTablet ? 570 : 745, isTablet ? 171 : 122],
-      to: [isTablet ? 744 : 944, isTablet ? 171 : 122],
+      from: [
+        isMobile ? 231 : isTablet ? 471 : 588,
+        isMobile ? 47 : isTablet ? 105 : 65,
+      ],
+      bend: [
+        isMobile ? 291 : isTablet ? 570 : 745,
+        isMobile ? 90 : isTablet ? 171 : 122,
+      ],
+      to: [
+        isMobile ? 373 : isTablet ? 744 : 944,
+        isMobile ? 90 : isTablet ? 171 : 122,
+      ],
     },
     {
-      from: [isTablet ? 472 : 591, isTablet ? 120 : 80],
-      bend: [isTablet ? 630 : 780, isTablet ? 241 : 190],
-      to: [isTablet ? 744 : 944, isTablet ? 241 : 190],
+      from: [
+        isMobile ? 233 : isTablet ? 472 : 591,
+        isMobile ? 53 : isTablet ? 120 : 80,
+      ],
+      bend: [
+        isMobile ? 307 : isTablet ? 630 : 780,
+        isMobile ? 140 : isTablet ? 241 : 190,
+      ],
+      to: [
+        isMobile ? 373 : isTablet ? 744 : 944,
+        isMobile ? 140 : isTablet ? 241 : 190,
+      ],
     },
     {
-      from: [isTablet ? 474 : 590, isTablet ? 130 : 100],
-      bend: [isTablet ? 630 : 790, isTablet ? 311 : 260],
-      to: [isTablet ? 744 : 944, isTablet ? 311 : 260],
+      from: [
+        isMobile ? 234 : isTablet ? 474 : 590,
+        isMobile ? 60 : isTablet ? 130 : 100,
+      ],
+      bend: [
+        isMobile ? 300 : isTablet ? 630 : 790,
+        isMobile ? 190 : isTablet ? 311 : 260,
+      ],
+      to: [
+        isMobile ? 373 : isTablet ? 744 : 944,
+        isMobile ? 190 : isTablet ? 311 : 260,
+      ],
     },
     {
-      from: [isTablet ? 475 : 586, isTablet ? 140 : 120],
-      bend: [isTablet ? 610 : 785, isTablet ? 381 : 330],
-      to: [isTablet ? 743 : 944, isTablet ? 381 : 330],
+      from: [
+        isMobile ? 232 : isTablet ? 475 : 586,
+        isMobile ? 70 : isTablet ? 140 : 120,
+      ],
+      bend: [
+        isMobile ? 266 : isTablet ? 610 : 785,
+        isMobile ? 240 : isTablet ? 381 : 330,
+      ],
+      to: [
+        isMobile ? 373 : isTablet ? 743 : 944,
+        isMobile ? 240 : isTablet ? 381 : 330,
+      ],
     },
   ];
 
@@ -149,7 +257,7 @@ const renderCustomizedLabel = ({ name, value, index, isTablet, isMobile }) => {
           <circle
             cx={path.from[0]}
             cy={path.from[1]}
-            r={2}
+            r={isMobile ? 1 : 2}
             fill={`${name === "be-great" ? "#959FBD" : "#FCFCFD"}`}
           />
         </>
@@ -164,14 +272,14 @@ const renderCustomizedLabel = ({ name, value, index, isTablet, isMobile }) => {
               <image
                 href={gretelSvg}
                 x={label.x}
-                y={label.y - 26}
-                width={65}
-                height={23}
+                y={isMobile ? label.y - 23 : label.y - 26}
+                width={isMobile ? 50 : 65}
+                height={isMobile ? 20 : 23}
               />
               <text
                 x={label.x}
-                y={label.y + 21}
-                fontSize={isMobile || isTablet ? 18 : 20}
+                y={isMobile ? label.y + 16 : label.y + 21}
+                fontSize={isMobile ? 14 : isTablet ? 18 : 20}
                 textAnchor="start"
               >
                 <tspan
@@ -190,7 +298,7 @@ const renderCustomizedLabel = ({ name, value, index, isTablet, isMobile }) => {
           <text
             x={label.x}
             y={label.y}
-            fontSize={isMobile || isTablet ? 18 : 20}
+            fontSize={isMobile ? 14 : isTablet ? 18 : 20}
             textAnchor={textAnchor}
           >
             <tspan
@@ -229,26 +337,26 @@ export default function CustomPieChart({ data }) {
       <img
         src={sdvPieChartLogo}
         alt="SDV logo"
-        className="absolute top-[42%] md:top-[55%] z-10"
+        className="absolute top-[52%] md:top-[55%] z-10 w-[90px] h-[50px] md:w-[108px] md:h-[58px]"
         draggable={false}
       />
-      <PieChart width={isMobile ? 400 : isTablet ? 800 : 1000} height={550}>
+      <PieChart
+        width={isMobile ? 375 : isTablet ? 800 : 1000}
+        height={isMobile ? 300 : 550}
+      >
         <Pie
           data={sortedData}
           cx="50%"
           cy="50%"
-          outerRadius={isMobile ? 180 : isTablet ? 220 : 275}
+          outerRadius={isMobile ? 120 : isTablet ? 220 : 275}
           dataKey="percentage"
           startAngle={60}
           endAngle={-300}
           isAnimationActive={false}
           stroke="none"
           labelLine={false}
-          label={
-            !isMobile
-              ? (props) =>
-                  renderCustomizedLabel({ ...props, isTablet, isMobile })
-              : false
+          label={(props) =>
+            renderCustomizedLabel({ ...props, isTablet, isMobile })
           }
         >
           {sortedData.map((_, index) => (
@@ -257,39 +365,12 @@ export default function CustomPieChart({ data }) {
           <Label
             value={`${sortedData[0]?.percentage}%`}
             position="center"
-            fontSize={isMobile ? 28 : 38}
+            fontSize={isMobile ? 20 : 38}
             fontWeight="medium"
             fill="#000036"
-            dy={isMobile ? 110 : 120}
+            dy={isMobile ? 70 : 120}
           />
         </Pie>
-        {isMobile && (
-          <Legend
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-            content={() => (
-              <ul className="flex flex-wrap justify-center items-center mt-4">
-                {sortedData.map((entry, index) => (
-                  <li
-                    key={`legend-item-${index}`}
-                    className="flex flex-col items-center text-center mx-[9px] mb-1"
-                  >
-                    <div className="flex items-center">
-                      <span
-                        className="w-3 h-3 mr-2"
-                        style={{ backgroundColor: COLORS[index] }}
-                      />
-                      <span className="text-midnight-950">
-                        {`${entry.name} (${sortedData[index].percentage}%)`}
-                      </span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            )}
-          />
-        )}
       </PieChart>
     </div>
   );
