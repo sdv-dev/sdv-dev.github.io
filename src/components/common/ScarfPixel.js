ec6381b245df03d761593f0a2715317c5fe8ce02
pcarapic15 <predrag.carapic@bytex.ro>
2025-07-22 09:27:16 +0200

    Create Scarf pixel and put it in main layout

---
+++ src/components/common/ScarfPixel.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ScarfPixel = () => {
+  return (
+    <>
+      {typeof window !== "undefined" &&
+      window.location.hostname === "datacebo.com" ? (
+        <img
+          referrerpolicy="no-referrer-when-downgrade"
+          src="https://static.scarf.sh/a.png?x-pxid=af9a538f-69ef-4e05-8553-76897f21be21"
+        />
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default ScarfPixel;
