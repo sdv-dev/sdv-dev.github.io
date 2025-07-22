100772edbd0a6fd50d01f1aa0f6d4b53b41805ce
Predrag <predrag.carapic@bytex.ro>
2025-07-22 09:41:41 +0200

    Change hostname to match the website (#21)

--- src/components/common/ScarfPixel.js
+++ src/components/common/ScarfPixel.js
@@ -4,7 +4,7 @@ const ScarfPixel = () => {
   return (
     <>
       {typeof window !== "undefined" &&
-      window.location.hostname === "datacebo.com" ? (
+      window.location.hostname === "sdv.dev" ? (
         <img
           referrerpolicy="no-referrer-when-downgrade"
           src="https://static.scarf.sh/a.png?x-pxid=af9a538f-69ef-4e05-8553-76897f21be21"
