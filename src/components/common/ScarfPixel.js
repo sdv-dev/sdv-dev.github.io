a7576bd56d5db99318d7af53c3dfde88dfc6be16
Predrag <predrag.carapic@bytex.ro>
2025-07-23 13:54:15 +0200

    Add logic for routing in nav (#22)

--- src/components/common/ScarfPixel.js
+++ src/components/common/ScarfPixel.js
@@ -8,6 +8,7 @@ const ScarfPixel = () => {
         <img
           referrerpolicy="no-referrer-when-downgrade"
           src="https://static.scarf.sh/a.png?x-pxid=af9a538f-69ef-4e05-8553-76897f21be21"
+          alt="scarf"
         />
       ) : (
         <></>
