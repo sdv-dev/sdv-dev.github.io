12b4fc123f39e0ebe7991c590887b5247dfe99e8
pcarapic15 <predrag.carapic@bytex.ro>
2025-08-15 10:15:08 +0200

    Uncomment activeDateTag

--- src/components/sd-gym/SdGymContent.js
+++ src/components/sd-gym/SdGymContent.js
@@ -12,7 +12,7 @@ export default function SdGymContent() {
   const [rawData, setRawData] = useState([]);
   const [data, setData] = useState([]);
   const [dateTags, setDateTags] = useState([]);
-  // const activeDateTag = dateTags.find((dt) => dt.active);
+  const activeDateTag = dateTags.find((dt) => dt.active);
 
   const fileUrl =
     "https://raw.githubusercontent.com/sdv-dev/sdv-dev.github.io/gatsby-home/assets/sdgym-leaderboard-files/SDGym Monthly Run.xlsx";
