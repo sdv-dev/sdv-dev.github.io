1ed818d4f149048ced3047544411946a6f973ec9
Predrag <predrag@datacebo.com>
2025-09-01 12:23:50 +0200

    Add squid setup snippet (#36)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,16 @@
 import "/src/styles/fonts.css";
-import '/src/styles/global.css';
-import '/src/styles/navbar.scss';
-import '/src/styles/post.scss';
-require("prismjs/themes/prism-solarizedlight.css")
+import "/src/styles/global.css";
+import "/src/styles/navbar.scss";
+import "/src/styles/post.scss";
+require("prismjs/themes/prism-solarizedlight.css");
 require("prismjs/themes/prism-okaidia.css");
+
+export const onClientEntry = () => {
+  (function (squid) {
+    window.$quid || (window.$quid = {});
+    const script = document.createElement("script");
+    script.src = "https://app.asksquid.ai/tfs/" + squid + "/sdk";
+    script.async = true;
+    document.head.appendChild(script);
+  })("68b566f28c37224fe55a5e15");
+};
