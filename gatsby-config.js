37001732fcbb4c0e01b336b28ef7934888ea467d
pcarapic15 <predrag.carapic@bytex.ro>
2025-09-11 09:03:38 +0200

    Configure Contentful in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,13 @@ module.exports = {
       options: ghostConfig.production,
     },
     {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
