7636732f04f14f60e6e7fd2ff0fb580dd58b98b7
pcarapic15 <predrag@datacebo.com>
2025-12-11 11:25:47 +0100

    Redirect 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Link } from "gatsby";
-import { Article } from "../components/common";
+import { Helmet } from "react-helmet";
 
 const NotFoundPage = () => (
-  <Article>
-    <div className="container pt-48">
-      <article className="content text-center">
-        <h1 className="content-title">Error 404</h1>
-        <section className="content-body">
-          Page not found, <Link to="/">return home</Link> to start over
-        </section>
-      </article>
-    </div>
-  </Article>
+  <Helmet>
+    <meta httpEquiv="refresh" content="0; URL=https://datacebo.com/404/" />
+    <link rel="canonical" href="https://datacebo.com/404/" />
+  </Helmet>
 );
 
 export default NotFoundPage;
